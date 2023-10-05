ab7c52527d9ec44e9499af41f0daed4717252930
nonamebbx <melnicukartem321@gmail.com>
2023-10-05 11:03:10 +0200

    Update

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+document.createElement("ul"); 
+const list = document.querySelector("#ingredients");
+const elements = ingredients.map(ingredient => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  return item;
+});
