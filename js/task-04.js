fa78e614b4d2247305b31c91f6f773febbd1d8bd
nonamebbx <melnicukartem321@gmail.com>
2023-10-07 15:06:48 +0200

    update

--- js/task-04.js
+++ js/task-04.js
@@ -1,14 +1,17 @@
-const buttons = document.querySelectorAll('button'); // отримуємо всі кнопки з документу
-const counterValue = document.querySelector('#value'); // отримуємо елемент з id="value"
+const value = document.querySelector('#value');
+let counterValue = 0;
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        let value = parseInt(counterValue.textContent); // отримуємо поточне значення counterValue і перетворюємо його в число
-        if (button.classList.contains('increment')) { // якщо кнопка має клас increment
-            value++; // збільшуємо значення на одиницю
-        } else if (button.classList.contains('decrement')) { // якщо кнопка має клас decrement
-            value--; // зменшуємо значення на одиницю
-        }
-        counterValue.textContent = value; // записуємо нове значення в counterValue
-    });
-});
+const decrement = () => {
+    counterValue -= 1;
+    value.textContent = counterValue;
+}
+
+const increment = () => {
+    counterValue += 1;
+    value.textContent = counterValue;
+}
+
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+decrementBtn.addEventListener('click', decrement);
+incrementBtn.addEventListener('click', increment);
