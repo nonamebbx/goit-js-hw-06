4d42fbe33eb246eeddbbe05b6cd46c63e50c9915
nonamebbx <melnicukartem321@gmail.com>
2023-10-08 16:55:23 +0200

    update

--- js/task-05.js
+++ js/task-05.js
@@ -2,8 +2,11 @@ const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
 const onInputChange = (event) => {
-    nameOutput.textContent = event.currentTarget.value;
     if (event.currentTarget.value === '') {
         nameOutput.textContent = 'Anonymous';
+    } else {
+        nameOutput.textContent = event.currentTarget.value;
     }
 }
+
+nameInput.addEventListener('input', onInputChange);
