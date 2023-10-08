d6dcdce595322f29c8e4ac8bf5369043b75c62bc
nonamebbx <melnicukartem321@gmail.com>
2023-10-08 17:07:58 +0200

    New

---
+++ js/task-06.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('#validation-input');
+const inputLength = Number(input.dataset.length);
+
+input.addEventListener('blur', (event) => {
+    if (event.currentTarget.value.length === inputLength) {
+        input.classList.add('valid');
+        input.classList.remove('invalid');
+    } else {
+        input.classList.add('invalid');
+        input.classList.remove('valid');
+    }
+});
