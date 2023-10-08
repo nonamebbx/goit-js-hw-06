1e824642faa6ecb9198bcd4cba498fb2063021ff
nonamebbx <melnicukartem321@gmail.com>
2023-10-08 17:34:04 +0200

    new

--- js/task-07.js
+++ js/task-07.js
@@ -2,5 +2,5 @@ const text = document.querySelector('#text');
 const size = document.querySelector('#font-size-control');
 
 size.addEventListener('input', (event) => {
-    text.style.fontSize = event.currentTarget.value + 'px';
+    text.style.fontSize = `${event.currentTarget.value}px`;
 });
