5ac4a45b2d4c1bb9c38e0908e2939a84fc65da90
nonamebbx <melnicukartem321@gmail.com>
2023-10-08 21:43:36 +0200

    update with corrections

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,11 @@ const text = document.querySelector('#text');
 const size = document.querySelector('#font-size-control');
 
 size.addEventListener('input', (event) => {
+    text.style.transition = 'font-size 0.5s linear';
     text.style.fontSize = `${event.currentTarget.value}px`;
 });
-text.style.fontSize = `${size.value}px`;
+
+size.addEventListener('change', (event) => {
+    text.style.transition = 'none';
+    text.style.fontSize = `${event.currentTarget.value}px`;
+});
